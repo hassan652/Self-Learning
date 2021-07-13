4fd84a3869b2c3b38c014fedfe8cd5275af36f7e
M. Hassan Mahmood <hassanmahmood652@gmail.com>
2021-07-13 18:41:03 +0200

    Add Fizzbuzz coding interview task solution.

---
+++ Fizzbuzz coding task.py
@@ -0,0 +1,14 @@
+'''Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the number
+and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”.'''
+
+for i in range(1, 100):
+    if i % 15 == 0:
+        print('Fizzbuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
